037e8fed09ef1d596cc43b7366fe32c6a8df819a
Soeparno Enterprise <soeparno.corp@outlook.com>
2025-11-15 19:21:44 +0700

    config.js

---
+++ config.js
@@ -0,0 +1,1 @@
+/* config.js */ const CONFIG = { BUCKET_URL: 'https://storage.storadera.com/soeparnocorp/storage', // ganti sesuai endpoint Storadera lo USERNAME: 'gibson', API_KEY: 'PASTE_YOUR_API_KEY_HERE' };
